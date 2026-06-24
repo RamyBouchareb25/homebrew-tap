aea90ef2ca7d947e4a378fdcfcc56ee4abb46b72
Ramybouchareb25 <rami.bouchareb@etu.usthb.dz>
2026-06-24 11:53:43 +0100

    Test publish workflow

--- Formula/orbcert.rb
+++ Formula/orbcert.rb
@@ -5,6 +5,8 @@ class Orbcert < Formula
   sha256 "99e53297836c36e35efd10c78da658eca86d165179364e8c8426fba808fff9bc"
   license "MIT"
 
+  revision 1
+
   depends_on "go" => :build
   depends_on "kubernetes-cli"
   depends_on :macos
